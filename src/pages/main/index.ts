70d0e3a1ca40b3e45d654a5d3da0614aafa770ca
shamkolovich <shamkolovich2022@mail.ru>
2022-12-18 19:30:31 +0300

    feat: create categories and brands for choose

--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -1,4 +1,4 @@
-//import products from '../../constants/products.json';
+import productsJson from '../../constants/products.json';
 import * as func from '../../utils/index';
 import './main.scss';
 
@@ -9,7 +9,7 @@ export function createMain(): void {
     const container = func.createElement('container', 'main-container', 'container');
 
     const aside = createAsideBar();
-    const article = createCatalog();
+    const article = createCatalogOfProducts();
 
     container.append(aside, article);
     main.append(container);
@@ -35,19 +35,20 @@ function createFilters() {
     const ul = func.createElement('ul', 'aside-list');
 
     asideBar.forEach((item) => {
-        const li = func.createElement('li', 'aside-list__item');
+        const li = func.createElement('li', 'aside-list__item', 'product');
+        const productsList = createProductsList();
 
         const spanText = func.createElement('span', 'aside-list__text');
         spanText.textContent = item;
 
         li.append(spanText, func.createElement('span', 'aside-list__img', 'aside-list__img_no-active'));
-        ul.append(li);
+        ul.append(li, productsList);
     });
 
     return ul;
 }
 
-function createCatalog() {
+function createCatalogOfProducts() {
     const article = func.createElement('article', 'article');
     const h1 = func.createElement('h1', 'main__title');
     h1.textContent = 'Catalog';
@@ -65,4 +66,56 @@ function showFilters(e: Event) {
 
     span.classList.toggle('aside-list__img_no-active');
     span.classList.toggle('aside-list__img_active');
+}
+
+function createProductsList() {
+    const ul = func.createElement('ul', 'product-list');
+    const productsArr = createArrOfProducts(productsJson.products, asideBar);
+
+    productsArr.forEach((product) => {
+        const li = func.createElement('li', 'product-list__item');
+        console.log(product);
+        const checkbox = func.createElement('input', 'product-list__checkbox') as HTMLInputElement;
+        checkbox.type = 'checkbox';
+
+        const span = func.createElement('span', 'product-list__text');
+        // span.textContent = product;
+
+        li.append(checkbox, span);
+        ul.append(li);
+    });
+
+    return ul;
+}
+
+function createArrOfProducts(products: Products[], categories: string[]) {
+    const arr: Array<Set<string>> = [];
+
+    categories.forEach((category) => {
+        const currentCategory = category.toLowerCase();
+        const set: Set<string> = new Set();
+
+        products.forEach((product) => {
+            if (typeof product[currentCategory] === 'string') {
+                set.add(product[currentCategory]);
+            }
+        });
+
+        set.size ? arr.push(set) : false;
+    });
+    return arr;
+}
+
+interface Products {
+    brand: string;
+    category: string;
+    description: string;
+    discountPercentage: string;
+    id: number;
+    images: string[];
+    price: number;
+    rating: number;
+    stock: number;
+    thumbnail: string;
+    title: string;
 }
